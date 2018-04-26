6adb1ee0093b7634baf818e01267d38e1dc85dbf
Michael Wolowyk <michael.wolowyk@springernature.com>
2018-04-26 18:07:29 +0200

    Initial commit

---
+++ background.js
@@ -0,0 +1,59 @@
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // Send a message to the active tab
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    });
+});
+
+// This block is new!
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "open_new_tab" ) {
+            chrome.tabs.create({"url": request.url});
+        }
+    }
+);
+
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    switch(request.type) {
+        case "dom-loaded":
+            alert(request.data.myProperty);
+            break;
+    }
+    return true;
+});
+
+
+// listening for an event / one-time requests
+// coming from the popup
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    switch(request.type) {
+        case "color-divs":
+            colorDivs();
+            break;
+    }
+    return true;
+});
+
+// listening for an event / long-lived connections
+// coming from devtools
+chrome.extension.onConnect.addListener(function (port) {
+    port.onMessage.addListener(function (message) {
+        switch(port.name) {
+            case "color-divs-port":
+                colorDivs();
+                break;
+        }
+    });
+});
+
+// send a message to the content script
+var colorDivs = function() {
+    chrome.tabs.getSelected(null, function(tab){
+        chrome.tabs.sendMessage(tab.id, {type: "colors-div", color: "#f9ffbf"});
+        // setting a badge
+        chrome.browserAction.setBadgeText({text: "marked!"});
+    });
+}
---
+++ content.js
@@ -0,0 +1,79 @@
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "clicked_browser_action" ) {
+            var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+            console.log(firstHref);
+
+            // This line is new!
+            chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+        }
+    }
+);
+
+function makeAjaxCall(url, methodType, data, callback){
+    return $.ajax({
+        url: url,
+        method: methodType,
+        data: data,
+        dataType: "json"
+    });
+}
+
+function getEntities(text){
+    const url = 'http://model.dbpedia-spotlight.org/en/annotate';
+    const data = {
+        confidence : 0.35,
+        text: text
+    };
+
+   return makeAjaxCall(url, 'GET', data);
+};
+
+function replaceTextWithSementities(respJson, innerHtml) {
+    const resourses = respJson.Resources;
+    const processedResources = [];
+    console.log('Response: ', respJson.Resources);
+    resourses.forEach(function (r) {
+        const resourceText = r['@surfaceForm'];
+        const resourceUri = r['@URI'];
+        if (processedResources.indexOf(resourceText) < 0) {
+
+            innerHtml = innerHtml.replace(" " + resourceText + " ", '<a class="sementity tooltip" href="' + resourceUri + '"> ' + resourceText + ' </a>');
+            processedResources.push(resourceText);
+        }
+
+    });
+    return innerHtml;
+}
+
+function createToolTips(){
+    var $sementity = $('.sementity');
+    $sementity.each(function(e, v){
+        var txt3 = document.createElement("span");
+        txt3.className = 'tooltiptext';
+        txt3.innerText = v.href.replace('http://dbpedia.org/resource/', ':');
+        v.append(txt3);
+    });
+};
+
+chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
+    switch(message.type) {
+        case "colors-div":
+            var selector = $('.richtext');
+            var innerHtml = selector.html();
+            selector.each(function(i, v){
+                const text = v.innerText.replace(/(?:\r\n|\r|\n|")/g, ' ');
+                getEntities(text).then(function(respJson){
+                    innerHtml = replaceTextWithSementities(respJson, innerHtml);
+                    selector.html(innerHtml);
+
+                    createToolTips();
+
+                }, function(reason){
+                    console.error("error in processing your request", reason);
+                });
+            });
+            break;
+    }
+});
